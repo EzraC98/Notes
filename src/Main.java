942dafd38eecc65ae83404c02077034127dd2697
100139516 <100139516@lubbockisd.net>
2023-09-05 10:01:58 -0500

    .

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        }
+    }
+}
+// variable -- memory location that holds a value
+//data types -- mission critical
+
+//Identifier rules
+// --ALWAYS start with a letter
+// NO spaces
+//CAN use numbers
+// -- _ is the only symbol allowed
+// CANNOT use Java keywords
+ // General conventions
+// -- classes begin with a capital
+// --camelCase is common for variableNames
+// --use identifiers that mean something
+
+// Data types
+// -primatives: store a single, simple value
+// 8: 4 integers, 2 decimals, 1 boolean (t/f), 1 character
+// -- Integers: byte, short, int, long.
+//    bits: 8     16    32    64
+//   byte: -128 to 127
+// short : -32768 to 32767
+// int: -21474483648 to 2147483647
+// long: -4quintillion to 4quintillion
